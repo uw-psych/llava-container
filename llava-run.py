4f2ef1557092f22ce2ca6ef35286b6e1111b0b93
Altan Orhon <altan@uw.edu>
2024-01-24 15:31:55 -0800

    Fixed arguments

--- llava-run.py
+++ llava-run.py
@@ -168,7 +168,7 @@ def eval_model_multiple(args):
     for i in args.image_file:
         for q in args.query:
             outputs = eval_model_single(
-                tokenizer, model, image_processor, context_len, args
+                tokenizer, model, image_processor, context_len, q, args
             )
             if args.json:
                 print(json.dumps({"image": i, "query": q, "output": outputs}))
